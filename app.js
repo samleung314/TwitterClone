4319e8330cda0b92b12f8ff06d6677c73fa51160
samleung314 <samleung314@gmail.com>
2018-03-22 10:45:28 -0400

    working express and handlebars

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var exphbs  = require('handlebars');
+var hbs = require('handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var hbs = exphbs.create({ /* config */ });
 
-// Register `hbs.engine` with the Express app.
-app.engine('handlebars', hbs.engine);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
