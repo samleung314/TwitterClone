ac2357a00ee81292b51932766be6402f7e500f9e
samleung314 <samleung314@gmail.com>
2018-03-25 00:22:29 -0400

    login and logout

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../user/User');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'login' });
+});
+
+router.post('/', function (req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  console.log("Finding username: " + username + " password: " + password);
+  User.findOne({ username: username }, function (err, user) {
+    //can't find a user by username
+    if (err || !user) {
+      console.log("Can't find username");
+      res.status(200).json({
+        status: 'ERROR'
+      });
+    } else {
+      if (user.username == username && user.password == password) {
+        console.log(username + " login success!");
+        res.status(200).json({
+          status: 'OK'
+        });
+      } else {
+        //the username/password is wrong
+        console.log("No such username/password combination");
+        res.status(200).json({
+          status: 'ERROR'
+        });
+      }
+    }
+  });
 });
 
 module.exports = router;
--- routes/logout.js
+++ routes/logout.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'logout' });
 });
 
+router.post('/', function(req, res, next) {
+  res.status(200).json({
+    status: 'OK'
+  });
+});
+
 module.exports = router;
