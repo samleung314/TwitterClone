09d61f6674c3dbf015d4450c92e19d2894e2799b
samleung314 <samleung314@gmail.com>
2018-04-23 19:12:48 -0400

    make sure user logged in to add media

--- routes/media.js
+++ routes/media.js
@@ -14,6 +14,15 @@ var memcached = new Memcached('localhost:11211', { retries: 10 });
 //addmedia endpoint
 router.post('/addmedia', upload.single('content'), function (req, res) {
 
+    // check if logged in
+    if (typeof (req.cookies.username) == 'undefined') {
+        res.status(200).json({
+            status: 'error',
+            error: 'No current user'
+        });
+        return;
+    }
+
     var id = (new Date).getTime().toString(); //create unique id for each item in cassandra
     var content = req.file.buffer;
 
@@ -34,6 +43,33 @@ router.post('/addmedia', upload.single('content'), function (req, res) {
                 status: 'OK',
                 id: id
             });
+        }
+    });
+});
+
+router.get('/media/:id', (req, res, next) => {
+
+    var id = req.params.id;
+    console.log("Finding media: " + id);
+
+    var mimetype = mime.lookup(id);
+    res.set('Content-Type', mimetype);
+
+    const query = 'SELECT content FROM media WHERE id = ?';
+
+    const params = [id];
+    client.execute(query, params, { prepare: true }, function (err, result) {
+        if (err) {
+            console.log('**ERROR** in client.execute');
+            console.log(err);
+        }
+        else {
+            console.log('Get image success!');
+            res.end(new Buffer(result.rows[0].content), 'binary');
+            /*
+               res.send() insists on sticking charset into the content-type
+               res.end() can send data back with a 'binary' encoding
+           */
         }
     });
 });
